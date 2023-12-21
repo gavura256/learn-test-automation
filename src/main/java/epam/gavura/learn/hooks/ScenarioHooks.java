ed13213fa63d97a888a2ac5faf2edacc809b1b1b
Kostiantyn Gavura <kostiantyn_gavura@epam.com>
2023-12-21 20:26:47 +0100

    fix gradlew

--- src/main/java/epam/gavura/learn/hooks/ScenarioHooks.java
+++ src/main/java/epam/gavura/learn/hooks/ScenarioHooks.java
@@ -6,34 +6,56 @@ import epam.gavura.learn.clients.ui.WebDriverSingleton;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 
 import static epam.gavura.learn.constants.Groups.UI_SMOKE;
 
 @Slf4j
+@UtilityClass
 public class ScenarioHooks {
     @Before(UI_SMOKE)
     public static void setupMethod() {
-        WebDriverSingleton.getWebDriver()
-            .manage()
-            .window()
-            .maximize();
+        WebDriverSingleton.getWebDriver().manage().window().maximize();
     }
 
     @After(UI_SMOKE)
-    public void tearDownMethod(Scenario scenario) {
+    public static void tearDownMethod(Scenario scenario) {
         if (scenario.isFailed()) {
-            ReportPortal.emitLog(
-                scenario.getName(),
-                LogLevel.INFO.name(),
-                new Date(),
-                ((TakesScreenshot) WebDriverSingleton.getWebDriver()).getScreenshotAs(OutputType.FILE));
-        }
+            log.info("Scenario failed, taking screenshot");
+            File screenshot = ((TakesScreenshot) WebDriverSingleton.getWebDriver()).getScreenshotAs(OutputType.FILE);
+            String name = scenario.getName();
+            saveScreenshot(name);
+            sendScreenShotToReportPortal(name, screenshot);
 
-        WebDriverSingleton.quit();
+            WebDriverSingleton.quit();
+        }
+    }
+
+    private static void saveScreenshot(String scenarioName) {
+        try {
+            File screenshotFile = ((TakesScreenshot) WebDriverSingleton.getWebDriver()).getScreenshotAs(OutputType.FILE);
+            Path folderPath = Paths.get("target", "screenshots");
+            Files.createDirectories(folderPath);
+            String filePath = folderPath.resolve(scenarioName + "_" + RandomStringUtils.randomNumeric(5) + ".png").toString();
+            Files.move(screenshotFile.toPath(), Paths.get(filePath));
+            log.info("Screenshot saved: " + filePath);
+        } catch (IOException e) {
+            log.info("Error while saving screenshot: " + e.getMessage());
+        }
+    }
+
+    private static void sendScreenShotToReportPortal(String name, File screenshot) {
+        ReportPortal.emitLog(name, LogLevel.INFO.name(), new Date(), screenshot);
     }
 }
